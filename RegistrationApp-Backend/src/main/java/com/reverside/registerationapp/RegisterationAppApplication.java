171ffbe34e37e24684e98be73b3f1d8abdb358fd
MG <mmoroane@hotmail.com>
2023-02-16 08:51:50 +0200

    Init Commit

---
+++ RegistrationApp-Backend/src/main/java/com/reverside/registerationapp/RegisterationAppApplication.java
@@ -0,0 +1,13 @@
+package com.reverside.registerationapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegisterationAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegisterationAppApplication.class, args);
+	}
+
+}
