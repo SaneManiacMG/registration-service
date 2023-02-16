a63204700cd41520399d0648168cd7c38b688d2b
MG <mmoroane@hotmail.com>
2023-02-16 09:35:47 +0200

    Created user repository

---
+++ RegistrationApp-Backend/src/main/java/com/reverside/registerationapp/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.reverside.registerationapp.repositories;
+
+import com.reverside.registerationapp.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
