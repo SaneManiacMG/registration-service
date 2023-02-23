00f3d5d9fc7fea0d56f7a998f2846dc57254cf33
MG <mmoroane@hotmail.com>
2023-02-23 08:12:47 +0200

    Init config

--- Angular-Frontend/registration-login-ui/src/app/model/users.model.ts
+++
@@ -1,9 +0,0 @@
-export interface Users {
-    userId: number;
-    email: string;
-    password: string;
-    isHrVerified: boolean;
-    isPasswordChanged: boolean;
-    accountCreated: Date;
-    lastLogin: Date;
-}
